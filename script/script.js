7dfa3a779ebfb8466c30bba16d0f29dfbe47c7e3
Steven Abreu <steven.abreu@arconsis.com>
2016-05-16 21:22:21 +0200

    + navbar style changes when scrolling down

--- script/script.js
+++ script/script.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
   $("#down-arrow").click(function(){
-    $("#down-arrow").animate({ bottom: "10%" }, 200);
-    $("#down-arrow").animate({ bottom: "5%" }, 200);
+    var scrollToThis = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
+    $('html, body').animate({
+      scrollTop: scrollToThis
+    }, 2000);
   });
+
+  var opac = 0.0;
+  $(window).scroll(function(){
+    var firstSec = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
+    var scrollPos = $(window).scrollTop();
+
+    if (scrollPos >= firstSec && opac != 1.0) {
+      $('.navigation-bar-background').animate({
+        opacity: 1.0
+      }, 500);
+      $('.nested-list-item').css({
+        'background-color': '#555',
+        'border-bottom' : 'none'
+      });
+      opac = 1.0;
+      
+    } else if (scrollPos < firstSec && opac != 0.0) {
+      $('.navigation-bar-background').animate({
+        opacity: 0.0
+      }, 500);
+      $('.nested-list-item').css({
+        'background-color' : 'transparent',
+        'border-bottom' : '2px solid white'
+      });
+      opac = 0.0;
+    }
+});
 });
