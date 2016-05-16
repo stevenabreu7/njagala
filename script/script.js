2cd3527205d41f43e6587acfef66a6de5de53a1f
Steven Abreu <steven.abreu@arconsis.com>
2016-05-16 15:54:15 +0200

    initial commit - navbar and images

---
+++ script/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#down-arrow").click(function(){
+    $("#down-arrow").animate({ bottom: "10%" }, 200);
+    $("#down-arrow").animate({ bottom: "5%" }, 200);
+  });
+});
