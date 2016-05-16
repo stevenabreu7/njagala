0bc94f399dde87ac6ee494baabf42c06e630eb03
Steven Abreu <steven.abreu@arconsis.com>
2016-05-16 23:04:40 +0200

    (FIX) burger menu working again // all script into .js file

--- script/script.js
+++ script/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+
+  // $(document).click(function(event) {
+  //     var text = $(event.target);
+  //     console.log(text);
+  // });
+
+  //hide burger-menu as default
+  $(".burger-menu").hide(0);
+
+  //hide burger menu if active and navbar turns
+  //visible because window width changed
+  $(window).resize(function() {
+    if ($(window).width() > 1050) {
+       $(".burger-menu").hide(0);
+       $('#burger-image').show(0);
+    }
+  });
+
+  //show burger menu on click
+  $('#burger-image').click(function() {
+    $(".burger-menu").show(500);
+    $('#burger-image').hide(0);
+  });
+
+  //hide when back button was clicked
+  $('#back-arrow').click(function() {
+    $(".burger-menu").hide(500);
+    $('#burger-image').show(0);
+  });
+
+
+  // --------------- other ---------------
+
+  //scroll down when down-arrow is clicked
   $("#down-arrow").click(function(){
     var scrollToThis = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
     $('html, body').animate({
@@ -6,6 +40,7 @@ $(document).ready(function() {
     }, 2000);
   });
 
+  //change navbar style when scrolling
   var opac = 0.0;
   $(window).scroll(function(){
     var firstSec = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
@@ -20,7 +55,7 @@ $(document).ready(function() {
         'border-bottom' : 'none'
       });
       opac = 1.0;
-      
+
     } else if (scrollPos < firstSec && opac != 0.0) {
       $('.navigation-bar-background').animate({
         opacity: 0.0
@@ -31,5 +66,5 @@ $(document).ready(function() {
       });
       opac = 0.0;
     }
-});
+  });
 });
