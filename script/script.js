c3d9eef534530b2240aa7640bffdb417b82d7719
Steven Abreu <steven.abreu@arconsis.com>
2016-06-05 17:03:08 +0200

    + cleaner code, minor fixes

--- script/script.js
+++ script/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
-  // $(document).click(function(event) {
-  //     var text = $(event.target);
-  //     console.log(text);
-  // });
-
-  var scrollSpeed = 0.7; // => px/ms
+  var scrollSpeed = 1.0; // => px/ms
   var navAnimation = 500; //animation opacity
   var burgerAnimation = 200; //hide and show burger menu
 
@@ -14,8 +9,30 @@ $(document).ready(function() {
   //hide burger-menu as default
   $(".burger-menu").hide(0);
 
-  //hide burger menu if active and navbar turns
-  //visible because window width changed
+  //plugins
+  jQuery.fn.extend({
+    noDeco: function () {
+      $(this).css("text-decoration", "none");
+    },
+
+    yesDeco: function () {
+      $(this).css("text-decoration", "underline");
+    },
+
+    scrollTo: function(scrollToThis) {
+      var totalToScroll = scrollToThis - $(window).scrollTop();
+      var duration = Math.abs(totalToScroll / scrollSpeed);
+
+      $(".burger-menu").hide(burgerAnimation);
+      burgerMenuActive = false;
+
+      $(this).animate({
+        scrollTop: scrollToThis
+      }, duration);
+    }
+  });
+
+  //hide burger menu if active and navbar turns visible
   $(window).resize(function() {
     if ($(window).width() > 1050 && burgerMenuActive) {
       $(".burger-menu").hide(0);
@@ -29,6 +46,13 @@ $(document).ready(function() {
     burgerMenuActive = true;
   });
 
+  //hide burger menu when back button was clicked
+  $('#back-arrow').click(function() {
+    $(".burger-menu").hide(burgerAnimation);
+    burgerMenuActive = false;
+  });
+
+  // change bg image in nav-bar
   var clickCounter = 1;
   $('header').click(function(){
     if (clickCounter != 1) {
@@ -40,171 +64,63 @@ $(document).ready(function() {
     }
   });
 
-  //hide when back button was clicked
-  $('#back-arrow').click(function() {
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
+  $(document).click(function(event) {
+    var target = $(event.target);
+    var offsetForTargetDict = {
+      '.down-arrow' : '.rest-of-page',
+      '.project-link' : '.about-njagala-section',
+      '.about-us-link' : '.about-us-section',
+      '.funding-link' : '.funding-section',
+      '.product-link' : '.product-section',
+      '.contact-link' : '.contact-section'
+    }
+
+    for (var tg in offsetForTargetDict) {
+      if (target.is(tg)) {
+        var toScroll = $(offsetForTargetDict[tg]).offset().top - $('.navigation-bar-wrapper').height();
+        $('html, body').scrollTo(toScroll);
+      }
+    }
   });
 
-  //scroll down when down-arrow is clicked
-  $("#down-arrow").click(function(){
-    var scrollToThis = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-  });
-
-  //scroll down when project-link is clicked
-  $(".project-link").click(function(){
-    var scrollToThis = $('.about-njagala-section').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
-  });
-
-  //scroll down when about us link is clicked
-  $(".about-us-link").click(function(){
-    var scrollToThis = $('.about-us-section').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
-  });
-
-  //scroll down when funding link is clicked
-  $(".funding-link").click(function(){
-    var scrollToThis = $('.funding-section').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
-  });
-
-  //scroll down when funding link is clicked
-  $(".product-link").click(function(){
-    var scrollToThis = $('.product-section').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
-  });
-
-  //scroll down when contact link is clicked
-  $(".contact-link").click(function(){
-    var scrollToThis = $('.contact-section').offset().top - $('.navigation-bar-wrapper').height();
-    var totalToScroll = scrollToThis - $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
-  });
-
-  //scroll down when home link is clicked
+  //scroll all the way up when home link is clicked
   $(".home-link").click(function(){
-    var scrollToThis = 0;
-    var totalToScroll = $(window).scrollTop();
-    var duration = Math.abs(totalToScroll / scrollSpeed);
-
-    $(".burger-menu").hide(burgerAnimation);
-    burgerMenuActive = false;
-
-    $('html, body').animate({
-      scrollTop: scrollToThis
-    }, duration);
-
+    $('html, body').scrollTo(0);
   });
 
   //scroll function!
-  var opac = 0.0;
   $(window).scroll(function(){
 
     //underline active section
+    var scrollPosition = $(window).scrollTop();
     var firstSection = $('.about-njagala-section').offset().top - $('.navigation-bar-wrapper').height();
     var secondSection = $('.about-us-section').offset().top - $('.navigation-bar-wrapper').height();
     var thirdSection = $('.funding-section').offset().top - $('.navigation-bar-wrapper').height();
     var fourthSection = $('.product-section').offset().top - $('.navigation-bar-wrapper').height();
     var fifthSection = $('.contact-section').offset().top - $('.navigation-bar-wrapper').height();
-    var scrollPosition = $(window).scrollTop();
+    var allLinks = ".funding-link, .project-link, .about-us-link, .product-link, .contact-link";
 
-    if (scrollPosition >= fifthSection) {
-      $(".funding-link").css("text-decoration", "none");
-      $(".project-link").css("text-decoration", "none");
-      $(".about-us-link").css("text-decoration", "none");
-      $('.product-link').css('text-decoration', 'none');
-      //show relevant one
-      $(".contact-link").css("text-decoration", "underline");
+    if (scrollPosition < firstSection) {
+      $(allLinks).noDeco();
+    } else if (scrollPosition >= fifthSection) {
+      $(allLinks).noDeco();
+      $(".contact-link").yesDeco();
     } else if (scrollPosition >= fourthSection) {
-      $(".funding-link").css("text-decoration", "none");
-      $(".project-link").css("text-decoration", "none");
-      $(".about-us-link").css("text-decoration", "none");
-      $(".contact-link").css("text-decoration", "none");
-      //show relevant one
-      $('.product-link').css('text-decoration', 'underline');
+      $(allLinks).noDeco();
+      $('.product-link').yesDeco();
     } else if (scrollPosition >= thirdSection) {
-      $(".project-link").css("text-decoration", "none");
-      $(".about-us-link").css("text-decoration", "none");
-      $('.product-link').css('text-decoration', 'none');
-      $(".contact-link").css("text-decoration", "none");
-      //show relevant one
-      $(".funding-link").css("text-decoration", "underline");
+      $(allLinks).noDeco();
+      $(".funding-link").yesDeco();
     } else if (scrollPosition >= secondSection) {
-      $(".funding-link").css("text-decoration", "none");
-      $(".project-link").css("text-decoration", "none");
-      $('.product-link').css('text-decoration', 'none');
-      $(".contact-link").css("text-decoration", "none");
-      //show relevant one
-      $(".about-us-link").css("text-decoration", "underline");
+      $(allLinks).noDeco();
+      $(".about-us-link").yesDeco();
     } else if (scrollPosition >= firstSection) {
-      $(".funding-link").css("text-decoration", "none");
-      $(".about-us-link").css("text-decoration", "none");
-      $('.product-link').css('text-decoration', 'none');
-      $(".contact-link").css("text-decoration", "none");
-      //show relevant one
-      $(".project-link").css("text-decoration", "underline");
-    } else {
-      //underline none
-      $(".funding-link").css("text-decoration", "none");
-      $(".project-link").css("text-decoration", "none");
-      $(".about-us-link").css("text-decoration", "none");
-      $('.product-link').css('text-decoration', 'none');
-      $(".contact-link").css("text-decoration", "none");
+      $(allLinks).noDeco();
+      $(".project-link").yesDeco();
     }
 
     //change navbar style when scrolling
-    var firstSec = $('#rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
+    var firstSec = $('.rest-of-page').offset().top - $('.navigation-bar-wrapper').height();
     var scrollPos = $(window).scrollTop();
 
     if (scrollPos >= firstSec - 100 && scrollPos < firstSec + 0) {
@@ -213,11 +129,8 @@ $(document).ready(function() {
       $('.nested-list-item').css({ 'background-image': 'url(images/barkkcloth.png)' });
     } else if (scrollPos > firstSec + 100) {
       $('.navigation-bar-background').css('opacity', '1.0');
-      // $('.navigation-bar-background').css('background-color', '#777');
-      // $('.nested-list-item').css({ 'background-color': '#888', 'border-bottom' : 'none' });
     } else if (scrollPos < firstSec) {
       $('.navigation-bar-background').css('opacity', '0.0');
-      // $('.nested-list-item').css({ 'background-color': 'transparent', 'border-bottom' : '2px solid white' });
       $('.nested-list-item').css({ 'background': 'none'});
     }
   });
