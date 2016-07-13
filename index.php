ff1b20c87f0de3de706d14be7d7d87f413bf1fc4
Steven Abreu <steven.abreu@arconsis.com>
2016-07-14 01:35:40 +0200

    + added slideshow

--- index.php
+++ index.php
@@ -238,7 +238,41 @@
         <h2 id="about-header">Über Uns</h2>
         <hr>
         <div class="group-picture">
-          <img src="images/enactus-group.JPG">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <img src="images/enactus-group_1.JPG">
+            </div>
+            <div class="item">
+              <img src="images/enactus-group_2.JPG">
+              <div class="carousel-caption">
+                <h3></h3>
+              </div>
+            </div>
+            <div class="item">
+              <img src="images/enactus-group_3.JPG">
+              <div class="carousel-caption">
+                <h3></h3>
+              </div>
+            </div>
+          </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+          </a>
+        </div>
         </div>
         <div class="row nomarg">
           <div class="njagala-logo col-xs-6 col-sm-3 col-md-3 col-xs-offset-3 col-sm-offset-1 col-lg-offset-2">
